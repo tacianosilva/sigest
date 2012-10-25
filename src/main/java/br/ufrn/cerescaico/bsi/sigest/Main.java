e9212c2a6bb0a66d11c9e1c35c1d8a9f4b1e8feb
taciano <tacianosilva@gmail.com>
2012-10-25 11:39:13 -0300

    Configurando o pom para utilizar os dados do git.

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/Main.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/Main.java
@@ -11,61 +11,58 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * 
  * @author taciano
  */
 public class Main {
 
-    public static void main(String[] args) {
-        /*
-    	Evento evento = new Evento();
+	public static void main(String[] args) {
+		/*
+		 * Evento evento = new Evento();
+		 * 
+		 * evento.setNome("Jats 2012");
+		 * evento.setDescricao("Jornada de Atualização Tecnológica");
+		 * 
+		 * EventoBO eventoMB = new EventoBO(); eventoMB.inserir(evento);
+		 * 
+		 * System.out.println(System.getProperty("user.dir"));
+		 * 
+		 * List<Evento> eventos = eventoMB.listar();
+		 * 
+		 * for (Evento ev : eventos) { System.out.println(ev); }
+		 * 
+		 * long start = System.currentTimeMillis(); //Preparing parameters Map
+		 * parameters = new HashMap(); //parameters.put("ReportTitle",
+		 * "Address Report"); //parameters.put("DataFile",
+		 * "CustomBeanFactory.java - Bean Array");
+		 * 
+		 * JRBeanCollectionDataSource data = new
+		 * JRBeanCollectionDataSource(eventos);
+		 * 
+		 * System.err.println("Filling time : " + (System.currentTimeMillis() -
+		 * start));
+		 * 
+		 * String jasperFile = System.getProperty("user.dir")+
+		 * "/src/main/webapp/reports/listaEventos.jrxml";
+		 * 
+		 * JasperPrint jasperPrint; try { JasperReport jasperReport =
+		 * JasperCompileManager.compileReport(jasperFile); jasperPrint =
+		 * JasperFillManager.fillReport(jasperReport, parameters, data);
+		 * //executa o relatório JasperViewer.viewReport(jasperPrint); } catch
+		 * (JRException ex) {
+		 * Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex); }
+		 */
 
-        evento.setNome("Jats 2012");
-        evento.setDescricao("Jornada de Atualização Tecnológica");
+		/*
+		 * Preenche o relatório com os dados. Gera o arquivo
+		 * BibliotecaPessoal.jrprint
+		 */
+		// JasperFillManager.fillReportToFile( jasperFile, parametros, jrRS );
 
-        EventoBO eventoMB = new EventoBO();
-        eventoMB.inserir(evento);
-
-        System.out.println(System.getProperty("user.dir"));
-
-        List<Evento> eventos = eventoMB.listar();
-
-        for (Evento ev : eventos) {
-            System.out.println(ev);
-        }
-
-        long start = System.currentTimeMillis();
-        //Preparing parameters
-        Map parameters = new HashMap();
-        //parameters.put("ReportTitle", "Address Report");
-        //parameters.put("DataFile", "CustomBeanFactory.java - Bean Array");
-
-        JRBeanCollectionDataSource data = new JRBeanCollectionDataSource(eventos);
-
-        System.err.println("Filling time : " + (System.currentTimeMillis() - start));
-
-        String jasperFile = System.getProperty("user.dir")+"/src/main/webapp/reports/listaEventos.jrxml";
-        
-        JasperPrint jasperPrint;
-        try {
-            JasperReport jasperReport = JasperCompileManager.compileReport(jasperFile);
-            jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, data);
-            //executa o relatório
-            JasperViewer.viewReport(jasperPrint);
-        } catch (JRException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        */
-
-        /*
-         * Preenche o relatório com os dados. Gera o arquivo
-         * BibliotecaPessoal.jrprint
-         */
-        //JasperFillManager.fillReportToFile( jasperFile, parametros, jrRS );
-
-        /*
-         * Exporta para o formato PDF
-         */
-        //JasperExportManager.exportReportToPdfFile( "untitled_report_1.jrprint" );
-    }
+		/*
+		 * Exporta para o formato PDF
+		 */
+		// JasperExportManager.exportReportToPdfFile(
+		// "untitled_report_1.jrprint" );
+	}
 }
--- src/main/java/br/ufrn/cerescaico/bsi/sigest/Sigest.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/Sigest.java
@@ -4,42 +4,44 @@ import java.io.Serializable;
 
 /**
  * Fachada do sistema Sigest.
+ * 
  * @author Taciano de Morais Silva - tacianosilva@gmail.com
  * @version 25/10/2012, Taciano de Morais Silva - tacianosilva@gmail.com
  * @since 25/10/2012
  */
 public final class Sigest extends AbstractFacade implements Serializable {
-    
-    /**
+
+	/**
 	 * serialVersionUID.
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
-     * Instância única da fachada.
-     */
-    private static volatile Sigest instance = null;
+	 * Instância única da fachada.
+	 */
+	private static volatile Sigest instance = null;
 
-    /**
-     * Construtor privado da fachada.
-     */
-    private Sigest() {
-    }
+	/**
+	 * Construtor privado da fachada.
+	 */
+	private Sigest() {
+	}
 
-    /**
-     * Retorna a instância única do singleton Sigest (fachada).
-     * @return A instância do Sepe.
-     */
-    public static Sigest getInstance() {
-        if (instance == null) {
-            synchronized (Sigest.class) {
-            	Sigest inst = instance;
-                if (inst == null) {
-                    instance = new Sigest();
-                }
-            }
-        }
-        return instance;
-    }
-    
+	/**
+	 * Retorna a instância única do singleton Sigest (fachada).
+	 * 
+	 * @return A instância do Sepe.
+	 */
+	public static Sigest getInstance() {
+		if (instance == null) {
+			synchronized (Sigest.class) {
+				Sigest inst = instance;
+				if (inst == null) {
+					instance = new Sigest();
+				}
+			}
+		}
+		return instance;
+	}
+
 }
