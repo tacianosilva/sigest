c8e8f9b5f9f03b298f2e1ad208c252c64b6998e2
taciano <tacianosilva@gmail.com>
2012-11-13 10:05:21 -0300

    Adicionando a classe Empresa e a Interface Bean ao modelo.

---
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/model/Bean.java
@@ -0,0 +1,23 @@
+package br.ufrn.cerescaico.bsi.sigest.model;
+
+import java.io.Serializable;
+
+/**
+ * @author Taciano Morais Silva
+ * @version 13/11/2012
+ * @since 13/11/2012
+ */
+public interface Bean extends Serializable {
+
+    /**
+     * Retorna o identificador do bean.
+     * @return O identificador do bean.
+     */
+    public Integer getId();
+
+    /**
+     * Modifica o identificador do bean.
+     * @param id O novo identificador do bean.
+     */
+    public void setId(Integer id);
+}
