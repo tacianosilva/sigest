9bf116bbff49e186fe39291ab640b72a974f0e8d
taciano <tacianosilva@gmail.com>
2012-11-29 18:26:29 -0300

    Adicionando novas classes para o modelo de dados.

---
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/model/Estagiario.java
@@ -0,0 +1,20 @@
+package br.ufrn.cerescaico.bsi.sigest.model;
+
+public class Estagiario implements Bean {
+
+	/**
+	 * Número de Serialização
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setId(Integer id) {
+		// TODO Auto-generated method stub	
+	}
+}
