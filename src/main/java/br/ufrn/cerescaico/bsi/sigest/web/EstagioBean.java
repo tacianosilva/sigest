637749df6e448938d6ba356d7f256a666d570df8
aragonense <aragonense@gmail.com>
2012-12-21 09:05:34 -0300

    COmmit com alterações de validação nas paginas, falta implementar alguns detalhes do Crud

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/web/EstagioBean.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/web/EstagioBean.java
@@ -1,74 +1,81 @@
 package br.ufrn.cerescaico.bsi.sigest.web;
 
-import br.ufrn.cerescaico.bsi.sigest.Sigest;
-import br.ufrn.cerescaico.bsi.sigest.bo.NegocioException;
-import br.ufrn.cerescaico.bsi.sigest.model.Estagio;
-
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
+import br.ufrn.cerescaico.bsi.sigest.Sigest;
+import br.ufrn.cerescaico.bsi.sigest.bo.NegocioException;
+import br.ufrn.cerescaico.bsi.sigest.model.Estagio;
+
 @ManagedBean(name = "estagioBean")
 @SessionScoped
 public class EstagioBean implements Serializable {
 
-    /**
+	/**
 	 * serialVersionUID.
 	 */
 	private static final long serialVersionUID = 1L;
 
 	private Sigest sigest = Sigest.getInstance();
-    
-    private List<Estagio> estagios = null;
-    
-    private Estagio estagio = null;
-    
-    
+
+	private List<Estagio> estagios = null;
+
+	private Estagio estagio = new Estagio();
 
 	public String incluir() {
-        FacesContext context = FacesContext.getCurrentInstance();
-           try {
-               sigest.cadastrarEstagio(getEstagio());
-               context.addMessage("Estágio cadastrado com sucesso!", new FacesMessage(FacesMessage.SEVERITY_INFO, "Estágio cadastrado com sucesso!",""));
-           } catch (NegocioException ex) {
-               context.addMessage("Erro ao cadastrar estágio. Tente novamente.", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao cadastrar estágio. Tente novamente.", ex.getMessage()));
-           }
-           return "";
-       }
-    
-    public String listar() {
-        try {
-            estagios = sigest.listarEstagios();
-        } catch (NegocioException ex) {
-            estagios = new ArrayList<Estagio>();
-            Logger.getLogger(EstagioBean.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
-            FacesContext context = FacesContext.getCurrentInstance();
-            context.addMessage("estagioBean.listar", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao carregar a lista de estagios", ex.getMessage()));
-        }
-        return "estagio";
-    }
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			sigest.cadastrarEstagio(getEstagio());
+			context.addMessage("Estágio cadastrado com sucesso!",
+					new FacesMessage(FacesMessage.SEVERITY_INFO,
+							"Estágio cadastrado com sucesso!", ""));
+		} catch (NegocioException ex) {
+			context.addMessage(
+					"Erro ao cadastrar estágio. Tente novamente.",
+					new FacesMessage(FacesMessage.SEVERITY_ERROR,
+							"Erro ao cadastrar estágio. Tente novamente.", ex
+									.getMessage()));
+		}
+		return "";
+	}
 
-    public List<Estagio> getEstagios() {
-    	listar();
-        return estagios;
-    }
-    
-    public String manter(){
-    	return "estagio.xhtml";
-    }
+	public String listar() {
+		try {
+			estagios = sigest.listarEstagios();
+		} catch (NegocioException ex) {
+			estagios = new ArrayList<Estagio>();
+			Logger.getLogger(EstagioBean.class.getName()).log(Level.SEVERE,
+					ex.getMessage(), ex);
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage("estagioBean.listar", new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Erro ao carregar a lista de estagios", ex.getMessage()));
+		}
+		return "estagio";
+	}
 
-    public void setEstagios(List<Estagio> estagios) {
-        this.estagios = estagios;
-    }
-    
-    public Estagio getEstagio() {
+	public List<Estagio> getEstagios() {
+		listar();
+		return estagios;
+	}
+
+	public String manter() {
+		return "estagio";
+	}
+
+	public void setEstagios(List<Estagio> estagios) {
+		this.estagios = estagios;
+	}
+
+	public Estagio getEstagio() {
 		return estagio;
 	}
 
