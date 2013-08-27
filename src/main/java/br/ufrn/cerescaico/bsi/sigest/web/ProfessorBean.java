f09d4da2c8e8e014bff30b4077affe0ffb51b8b5
Taciano Morais Silva <tacianosilva@gmail.com>
2013-08-27 15:50:37 -0300

    Modificações no Managed Bean de Professor para o funcionamento da inserção. Falta acrescentar a mensagem de sucesso!

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/web/ProfessorBean.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/web/ProfessorBean.java
@@ -8,11 +8,8 @@ import java.util.logging.Logger;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-
-import org.primefaces.component.splitbutton.SplitButton;
 
 import br.ufrn.cerescaico.bsi.sigest.Sigest;
 import br.ufrn.cerescaico.bsi.sigest.bo.NegocioException;
@@ -22,82 +19,107 @@ import br.ufrn.cerescaico.bsi.sigest.model.Professor;
 @ManagedBean(name = "professorBean")
 @SessionScoped
 public class ProfessorBean implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Sigest sigest = Sigest.getInstance();
-    
+    private Sigest sigest = Sigest.getInstance();
+
+    private String nome = null;
+
+    private Integer siape = null;
+
     private List<Professor> professores = null;
-    private Professor professor = new Professor();
-    private Curso curso = new Curso();
-    private Integer cursoId;
-    private String cursoToString;
-    
-    public String inserir(){
-    	try {
-    		//cursoId = Integer.parseInt(Split(cursoToString, "-"));
-    		//String[] pro = cursoToString.split("\\-");
-    		//cursoId = Integer.parseInt(pro[0]);
-    		//curso = sigest.buscarCurso(cursoId);
-    		//professor.setCurso(curso);
-            sigest.cadastrarProfessor(professor);
+
+    private Professor professor = null;
+    private Curso curso = null;
+
+    private Integer codigocurso = null;
+
+    public String manter() {
+        return "/professor/manter";
+    }
+
+    public String inserir() {
+        try {
+            sigest.cadastrarProfessor(getProfessor());
         } catch (NegocioException ex) {
-            Logger.getLogger(ProfessorBean.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            Logger.getLogger(ProfessorBean.class.getName()).log(Level.SEVERE,
+                    ex.getMessage(), ex);
             FacesContext context = FacesContext.getCurrentInstance();
-            context.addMessage("professorBean.inserir", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao cadastrar professor", ex.getMessage()));
+            context.addMessage("professorBean.inserir", new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR, "Erro ao cadastrar professor",
+                    ex.getMessage()));
         }
         return "manterProfessor";
     }
-    
+
     public String listar() {
         try {
             professores = sigest.listarProfessor();
         } catch (NegocioException ex) {
             professores = new ArrayList<Professor>();
-            Logger.getLogger(ProfessorBean.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            Logger.getLogger(ProfessorBean.class.getName()).log(Level.SEVERE,
+                    ex.getMessage(), ex);
             FacesContext context = FacesContext.getCurrentInstance();
-            context.addMessage("professorBean.listar", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao carregar a lista de professores", ex.getMessage()));
+            context.addMessage("professorBean.listar", new FacesMessage(
+                    FacesMessage.SEVERITY_ERROR,
+                    "Erro ao carregar a lista de professores", ex.getMessage()));
         }
         return "manterProfessor";
     }
-   
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getSiape() {
+        return siape;
+    }
+
+    public void setSiape(Integer siape) {
+        this.siape = siape;
+    }
+
     public List<Professor> getProfessores() {
-    	listar();
+        listar();
         return professores;
     }
 
     public void setProfessores(List<Professor> professores) {
         this.professores = professores;
     }
-    
-    public Professor getProfessor(){
-    	return professor;
+
+    public Professor getProfessor() throws NegocioException {
+        professor = new Professor();
+        professor.setNome(getNome());
+        professor.setSiape(getSiape());
+        professor.setCurso(getCurso());
+        return professor;
     }
-    
-    public void setProfessor(Professor professor){
-    	this.professor = professor;
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
     }
-    
-    public void setCurso(Curso curso){
-    	this.curso = curso;
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
     }
-    
-    public Curso getCurso(){
-    	return curso;
+
+    public Curso getCurso() throws NegocioException {
+        if (curso == null) {
+            curso = sigest.buscarCurso(getCodigocurso());
+        }
+        return curso;
     }
-    
-    public void setCursoId(Integer cursoId){
-    	this.cursoId = cursoId;
+
+    public Integer getCodigocurso() {
+        return codigocurso;
     }
-    
-    public Integer getCursoId(){
-    	return cursoId;
-    }
-    
-    public void setCursoToString(String cursoToString){
-    	this.cursoToString = cursoToString;
-    }
-    
-    public String getCursoToString(){
-    	return cursoToString;
+
+    public void setCodigocurso(Integer codigocurso) {
+        this.codigocurso = codigocurso;
     }
 }
