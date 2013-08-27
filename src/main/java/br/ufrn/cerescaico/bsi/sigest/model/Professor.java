0b65e7b8e3309c2c316dd9ff9d4094dfbf011ba6
Taciano Morais Silva <tacianosilva@gmail.com>
2013-08-27 15:48:59 -0300

    Adicionamos a informação de alocação na sequência e removemos os tabs.

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/model/Professor.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/model/Professor.java
@@ -4,94 +4,93 @@ import java.io.Serializable;
 import javax.persistence.*;
 import java.util.List;
 
-
 /**
  * The persistent class for the professor database table.
- * 
+ *
  */
 @Entity
 @Table(name="professor")
 public class Professor implements Serializable, Bean {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@SequenceGenerator(name="PROFESSOR_CODIGO_GENERATOR", sequenceName="PROFESSOR_CODIGO_SEQ")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PROFESSOR_CODIGO_GENERATOR")
-	@Column(unique=true, nullable=false)
-	private Integer codigo;
+    @Id
+    @SequenceGenerator(name="PROFESSOR_CODIGO_GENERATOR", sequenceName="PROFESSOR_CODIGO_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PROFESSOR_CODIGO_GENERATOR")
+    @Column(unique=true, nullable=false)
+    private Integer codigo;
 
-	@Column(nullable=false, length=255)
-	private String nome;
+    @Column(nullable=false, length=255)
+    private String nome;
 
-	@Column(nullable=false)
-	private Integer siape;
+    @Column(nullable=false)
+    private Integer siape;
 
-	//bi-directional many-to-one association to Avaliacao
-	@OneToMany(mappedBy="professor")
-	private List<Avaliacao> avaliacaos;
+    //bi-directional many-to-one association to Avaliacao
+    @OneToMany(mappedBy="professor")
+    private List<Avaliacao> avaliacaos;
 
-	//bi-directional many-to-one association to Estagio
-	@OneToMany(mappedBy="professor")
-	private List<Estagio> estagios;
+    //bi-directional many-to-one association to Estagio
+    @OneToMany(mappedBy="professor")
+    private List<Estagio> estagios;
 
-	//bi-directional many-to-one association to Curso
+    //bi-directional many-to-one association to Curso
     @ManyToOne
-	@JoinColumn(name="curso", nullable=false)
-	private Curso curso;
+    @JoinColumn(name="curso", nullable=false)
+    private Curso curso;
 
     public Professor() {
     }
 
-	public Professor(int i, int j, String string) {
-		// TODO Auto-generated constructor stub
-	}
+    public Professor(int i, int j, String string) {
+        // TODO Auto-generated constructor stub
+    }
 
-	public Integer getCodigo() {
-		return this.codigo;
-	}
+    public Integer getCodigo() {
+        return this.codigo;
+    }
 
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
-	}
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
 
-	public String getNome() {
-		return this.nome;
-	}
+    public String getNome() {
+        return this.nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public Integer getSiape() {
-		return this.siape;
-	}
+    public Integer getSiape() {
+        return this.siape;
+    }
 
-	public void setSiape(Integer siape) {
-		this.siape = siape;
-	}
+    public void setSiape(Integer siape) {
+        this.siape = siape;
+    }
 
-	public List<Avaliacao> getAvaliacaos() {
-		return this.avaliacaos;
-	}
+    public List<Avaliacao> getAvaliacaos() {
+        return this.avaliacaos;
+    }
 
-	public void setAvaliacaos(List<Avaliacao> avaliacaos) {
-		this.avaliacaos = avaliacaos;
-	}
-	
-	public List<Estagio> getEstagios() {
-		return this.estagios;
-	}
+    public void setAvaliacaos(List<Avaliacao> avaliacaos) {
+        this.avaliacaos = avaliacaos;
+    }
 
-	public void setEstagios(List<Estagio> estagios) {
-		this.estagios = estagios;
-	}
-	
-	public Curso getCurso() {
-		return this.curso;
-	}
+    public List<Estagio> getEstagios() {
+        return this.estagios;
+    }
 
-	public void setCurso(Curso cursoBean) {
-		this.curso = cursoBean;
-	}
-	
+    public void setEstagios(List<Estagio> estagios) {
+        this.estagios = estagios;
+    }
+
+    public Curso getCurso() {
+        return this.curso;
+    }
+
+    public void setCurso(Curso cursoBean) {
+        this.curso = cursoBean;
+    }
+
 }
