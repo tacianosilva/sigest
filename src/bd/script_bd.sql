e314f5166b5538d4c0c02f984f9970efdd8e1e3a
Taciano Morais Silva <tacianosilva@gmail.com>
2013-09-13 11:51:10 -0300

    Adicionando nova coluna na tabela usuário.

--- src/bd/script_bd.sql
+++ src/bd/script_bd.sql
@@ -3,6 +3,7 @@ CREATE TABLE usuario (
                 nome VARCHAR(255) NOT NULL,
                 email VARCHAR(255) NOT NULL,
                 senha VARCHAR(255) NOT NULL,
+                ativo BOOLEAN NOT NULL DEFAULT false,
                 CONSTRAINT usuario_pk PRIMARY KEY (codigo)
 );
 
