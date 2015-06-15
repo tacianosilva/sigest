7d6e3f46d2e3e5c11aa3df5b8eaa3e05597bbbfb
Taciano Morais Silva <tacianosilva@gmail.com>
2015-06-15 15:50:47 -0300

    Adicionando os métodos hashCode e equals na classe Curso.

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/model/Curso.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/model/Curso.java
@@ -101,4 +101,34 @@ public class Curso implements Serializable, Bean {
         return getCodigo() + " - " + getNome();
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Curso))
+            return false;
+        Curso other = (Curso) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        return true;
+    }
 }
