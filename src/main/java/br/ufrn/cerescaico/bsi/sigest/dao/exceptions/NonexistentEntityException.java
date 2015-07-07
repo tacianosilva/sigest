c28d3233eeb55815b9de5162fe355c2e6c9d0436
Taciano Morais Silva <tacianosilva@gmail.com>
2015-07-07 00:00:12 -0300

    Add serial id!

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/dao/exceptions/NonexistentEntityException.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/dao/exceptions/NonexistentEntityException.java
@@ -1,9 +1,16 @@
 package br.ufrn.cerescaico.bsi.sigest.dao.exceptions;
 
 public class NonexistentEntityException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7699206043578165485L;
+
     public NonexistentEntityException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public NonexistentEntityException(String message) {
         super(message);
     }
