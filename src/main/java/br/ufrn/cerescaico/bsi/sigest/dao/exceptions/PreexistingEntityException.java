4b3ff7b9c9b847af5ab10d7ffe60c437fb774b94
Taciano Morais Silva <tacianosilva@gmail.com>
2015-07-08 17:07:07 -0300

    Pequenas formatações!

--- src/main/java/br/ufrn/cerescaico/bsi/sigest/dao/exceptions/PreexistingEntityException.java
+++ src/main/java/br/ufrn/cerescaico/bsi/sigest/dao/exceptions/PreexistingEntityException.java
@@ -1,9 +1,16 @@
 package br.ufrn.cerescaico.bsi.sigest.dao.exceptions;
 
 public class PreexistingEntityException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6095493296186872200L;
+
     public PreexistingEntityException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public PreexistingEntityException(String message) {
         super(message);
     }
